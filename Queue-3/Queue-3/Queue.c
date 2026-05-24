f7b78bbcdb012e434d009a61e5c4260f936b35d7
lzh65038-web <lzh65038@gmail.com>
2026-05-24 23:48:16 +0800

    Stack && Queue
    复现栈和队列

---
+++ Queue-3/Queue-3/Queue.c
@@ -0,0 +1,104 @@
+﻿#include "Queue.h"
+
+// 初始化队列 
+void QueueInit(Queue* q)
+{
+	assert(q);
+	q->_front = NULL;
+	q->_rear = NULL;
+}
+
+// 队尾入队列 
+void QueuePush(Queue* q, QDataType data)
+{
+	assert(q);
+
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));
+	if (newnode == NULL)
+	{
+		perror("malloc failed!\n");
+		exit(1);
+	}
+	newnode->_data = data;
+	newnode->_next = NULL;
+
+	if (q->_front == NULL)
+	{
+		q->_front = q->_rear = newnode;
+	}
+	else
+	{
+		q->_rear->_next = newnode;
+		q->_rear = newnode;
+	}
+
+}
+
+// 队头出队列 
+void QueuePop(Queue* q)
+{
+	assert(q);
+	assert(q->_front);
+
+	QNode* next = q->_front->_next;
+	free(q->_front);
+	q->_front = next;
+
+	if (q->_front == NULL) q->_rear = NULL;
+}
+
+// 获取队列头部元素 
+QDataType QueueFront(Queue* q)
+{
+	assert(q);
+	assert(q->_front);
+
+	return q->_front->_data;
+}
+
+// 获取队列队尾元素 
+QDataType QueueBack(Queue* q)
+{
+	assert(q);
+	assert(q->_front);
+
+	return q->_rear->_data;
+}
+
+// 获取队列中有效元素个数 
+int QueueSize(Queue* q)
+{
+	assert(q);
+
+	QNode* pcur = q->_front;
+	int size = 0;
+	while (pcur)
+	{
+		++size;
+		pcur = pcur->_next;
+	}
+	return size;
+}
+// 检测队列是否为空，如果为空返回非零结果，如果非空返回0 
+bool QueueEmpty(Queue* q)
+{
+	assert(q);
+	return q->_front == NULL;
+}
+
+// 销毁队列 
+void QueueDestroy(Queue* q)
+{
+	assert(q);
+
+	QNode* pcur = q->_front;
+	while (pcur)
+	{
+		QNode* next = pcur->_next;
+		free(pcur);
+		pcur = next;
+	}
+	q->_front = NULL;
+	q->_rear = NULL;
+
+}
---
+++ Stack-4/Stack-4/Test.c
@@ -0,0 +1,28 @@
+﻿#include "Stack.h"
+
+void Test1()
+{
+	Stack st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+
+	StackDestroy(&st);
+
+}
+
+
+int main()
+{
+	Test1();
+	return 0;
+}
